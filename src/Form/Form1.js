26403b722532affb1b55b0666ce108dd72d2e2bd
fitrianinasir <fitrianinasir8@gmail.com>
2019-08-01 22:56:58 +0800

    third commit

--- src/Form/Form1.js
+++ src/Form/Form1.js
@@ -1,73 +1,115 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 import './Form.css';
 import NavigationBar from '../Navbar/Navbar';
+import { ValidationForm, TextInput} from "react-bootstrap4-form-validation";
 class Form1 extends Component{
+  constructor(props){
+    super(props);
+    // If you want to use the reset state function, you need to have a reference to the ValidationForm component
+    //If your React < 16.3, check https://reactjs.org/docs/refs-and-the-dom.html#callback-refs
+    this.formRef = React.createRef();
+    this.state = {
+        immediate:true,
+        setFocusOnError:true,
+        clearInputOnReset:false
+    }
+  }
+
+  handleSubmit = (e, formData, inputs) => {
+      e.preventDefault();
+      console.log(formData);
+      alert(JSON.stringify(formData, null, 2));
+  }
+
+  handleErrorSubmit = (e,formData, errorInputs) => {
+      console.log(e,formData, errorInputs)
+  }
+
   render(){
     return(
       <div>
         <NavigationBar navbartitle="Pengenalan Tempat"/>
+        <ValidationForm onSubmit={this.handleSubmit} onErrorSubmit={this.handleErrorSubmit}
+                        ref={this.formRef}
+                        immediate={this.state.immediate}
+                        setFocusOnError={this.state.setFocusOnError}
+                        defaultErrorMessage={{ required: "Please enter something."}} >
         <div className="form-page-content">
           <div className="form-page-group">
             <label htmlFor="">Provinsi</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11" 
+              name="provinsi"
+              className="form-control col-sm-11 input-text" 
               placeholder="Nama Provinsi"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Kabupaten/Kota</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11 col-sm-6"
+              name="kabupatenkota"
+              className="form-control col-sm-11 input-text"
               placeholder="Kabupaten/Kota"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Kecamatan</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="kecamatan"
+              className="form-control col-sm-11 input-text"
               placeholder="Kecamatan"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Desa/Kelurahan</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="desa"
+              className="form-control col-sm-11 input-text"
               placeholder="Desa/Kelurahan"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Dusun</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="dusun"
+              className="form-control col-sm-11 input-text"
               placeholder="Dusun"  
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">RT/RW</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="rtrw"
+              className="form-control col-sm-11 input-text"
               placeholder="RT/RW"  
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">GPS Koordinat</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="gps"
+              className="form-control col-sm-11 input-text"
               placeholder="Koordinat Lokasi"
+              required
             />
           </div>
           <div className="form-group">
-            <Link to="/form2"><button className="btn primary">SIMPAN</button></Link>
+            <button className="btn primary">SIMPAN</button>
           </div>
         </div>
+        </ValidationForm>
       </div>
     );
   }
--- src/Form/Form2.js
+++ src/Form/Form2.js
@@ -1,48 +1,83 @@
 import React, {Component} from 'react';
 import NavigationBar from '../Navbar/Navbar'
-import {Link} from 'react-router-dom';
+import { ValidationForm, TextInput} from "react-bootstrap4-form-validation";
 class Form1 extends Component{
+  constructor(props){
+    super(props);
+    // If you want to use the reset state function, you need to have a reference to the ValidationForm component
+    //If your React < 16.3, check https://reactjs.org/docs/refs-and-the-dom.html#callback-refs
+    this.formRef = React.createRef();
+    this.state = {
+        immediate:true,
+        setFocusOnError:true,
+        clearInputOnReset:false
+    }
+    }
+
+    handleSubmit = (e, formData, inputs) => {
+        e.preventDefault();
+        console.log(formData);
+        alert(JSON.stringify(formData, null, 2));
+    }
+
+    handleErrorSubmit = (e,formData, errorInputs) => {
+        console.log(e,formData, errorInputs)
+    }
   render(){
     return(
       <div>
       <NavigationBar navbartitle="Keterangan Rumah Tangga"/>
+      <ValidationForm onSubmit={this.handleSubmit} onErrorSubmit={this.handleErrorSubmit}
+                        ref={this.formRef}
+                        immediate={this.state.immediate}
+                        setFocusOnError={this.state.setFocusOnError}
+                        defaultErrorMessage={{ required: "Please enter something."}} >
         <div className="form-page-content">
           <div className="form-page-group">
             <label htmlFor="">Nama Kepala Rumah Tangga</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11" 
+              name="kepalaRt"
+              className="form-control col-sm-11 input-text" 
               placeholder="Nama"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">No. Kartu Keluarga</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="kk"
+              className="form-control col-sm-11 input-text"
               placeholder="00"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Jumlah Anggota Rumah Tangga</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="anggota"
+              className="form-control col-sm-11 input-text"
               placeholder="00"
+              required
             />
           </div>
           <div className="form-page-group">
             <label htmlFor="">Banyaknya Balita(0-59)</label>
-            <input 
+            <TextInput 
               type="text" 
-              className="form-control col-sm-11"
+              name="balita"
+              className="form-control col-sm-11 input-text"
               placeholder="00"
+              required
             />
           </div>
           <div className="form-group">
-            <Link to="/soal"><button className="btn2 primary btn">SIMPAN</button></Link>
+            <button className="btn2 primary btn">SIMPAN</button>
           </div>
         </div>
+        </ValidationForm>
       </div>
     );
   }
